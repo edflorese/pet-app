af373fc71cd2ccd768401fb4c8482bb87dcee5fb
edflorese <edflorese@uce.edu.ec>
2025-01-19 21:34:30 -0500

    feat: :sparkles: Login page added

---
+++ app/index.jsx
@@ -0,0 +1,12 @@
+import { Link } from "expo-router";
+import { Pressable, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={{ flex: 1 }}>
+      <Link href={"/login"}>
+        <Text>Go to Login Screen</Text>
+      </Link>
+    </View>
+  );
+}
--- app/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function Index() {
-  return (
-    <View style={{flex:1}}>
-      <Text style={{fontFamily:'outfit-bold', fontSize:40}}>Test of font !</Text>
-    </View>
-  )
-}
