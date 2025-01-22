01360d3d960dbcba17c60601c8b0ffe58b057c4a
edflorese <edflorese@uce.edu.ec>
2025-01-21 23:38:47 -0500

    feat: :sparkles: Auth of login implemented

--- app/index.jsx
+++ app/index.jsx
@@ -1,7 +1,17 @@
-import { Link } from "expo-router";
+import { Link, useRootNavigationState } from "expo-router";
+import { useEffect } from "react";
 import { Pressable, Text, View } from "react-native";
 
 export default function Index() {
+  const rootNavigationState = useRootNavigationState();
+  useEffect(() => {
+    CheckNavLoaded();
+  }, []);
+  const CheckNavLoaded = () => {
+    if (!rootNavigationState.key) {
+      return null;
+    }
+  };
   return (
     <View style={{ flex: 1 }}>
       <Link href={"/login"}>
