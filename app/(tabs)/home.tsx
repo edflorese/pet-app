45bcacb0faa3f31abb7fe277d64c7d98bffce31b
edflorese <edflorese@uce.edu.ec>
2025-01-23 16:42:14 -0500

    feat: :lipstick: Component to add pets was created

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,24 +1,57 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import Header from '@/components/Home/Header'
-import Slider from '@/components/Home/Slider'
-import PetListByCategory from '@/components/Home/PetListByCategory'
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import React from "react";
+import Header from "@/components/Home/Header";
+import Slider from "@/components/Home/Slider";
+import PetListByCategory from "@/components/Home/PetListByCategory";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import Colors from "@/constants/Colors";
 
 export default function Home() {
   return (
-    <View style={{
-            padding: 20,
-            marginTop: 20,
-          }}>
+    <View
+      style={{
+        padding: 20,
+        marginTop: 20,
+      }}
+    >
       {/* Header */}
-        <Header/>
+      <Header />
       {/* Slider */}
-          <Slider />
+      <Slider />
       {/* Category */}
-          <PetListByCategory />
+      <PetListByCategory />
       {/* {List of Pets} */}
 
       {/* Add a New Pet Option */}
+      <TouchableOpacity style={styles.addNewContainer}>
+        <MaterialIcons name="pets" size={24} color={Colors.PRIMARY} />
+        <Text
+          style={{
+            fontFamily: "outfit-medium",
+            color: Colors.PRIMARY,
+            fontSize: 18,
+          }}
+        >
+          Add a new pet
+        </Text>
+      </TouchableOpacity>
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  addNewContainer: {
+    display: "flex",
+    flexDirection: "row",
+    gap: 10,
+    alignItems: "center",
+    padding: 20,
+    marginTop: 20,
+    backgroundColor: Colors.LIGHT_PRIMARY,
+    borderWidth: 1,
+    borderColor: Colors.PRIMARY,
+    borderRadius: 15,
+    borderStyle: "dashed",
+    justifyContent: "center",
+  },
+});
